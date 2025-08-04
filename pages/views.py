b560ec500f3c3a2565d423c02e8bccc8a873ef5b
prince-cloud <acheampongprince227@gmail.com>
2025-08-04 13:39:58 +0000

    refactor: ui refactoring

--- pages/views.py
+++ pages/views.py
@@ -1,9 +1,14 @@
 from django.views.generic import TemplateView
+from django.shortcuts import redirect
 
 
 class HomePageView(TemplateView):
     template_name = "pages/home.html"
 
 
-class AboutPageView(TemplateView):
-    template_name = "pages/about.html"
+# class AboutPageView(TemplateView):
+# template_name = "pages/about.html"
+
+
+def about(request):
+    return redirect("home")
